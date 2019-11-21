e3d39844fa813d0a0a74ac26d4daa75d0e4cd8c5
Front-end Developer <vanderleigomes.silva@hotmail.com>
2019-11-21 14:37:46 -0300

    .

--- src/components/navbar/DefaultNavbar.vue
+++ src/components/navbar/DefaultNavbar.vue
@@ -1,24 +1,38 @@
 <template>
     <div class="navbar" :class="navPosition">
-        <span>navbar</span>
+        <div class="logo">
+            <a href="">
+                <img src="" alt="">
+            </a>
+        </div>
+        <div class="nav">
+            <ul>
+                <li>item</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     props:{
-        navPosition:{
+        position:{
             type: String,
             default: 'top'
+        },
+        logo:{
+            type: String,
+            required: true
+        },
+        navItems:{
+            type: Array
         }
     }
 }
 </script>
 
 <style lang="scss">
-    .navbar{
-
-    }
+    .navbar{}
     .navbar.top{}
     .navbar.left{}
 </style>
